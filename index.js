5c751a09e1e2bcd482840a59a6575da5fb7078be
docarmo013 <gustavosantos2009g@gmail.com>
2024-08-05 11:29:52 -0300

    versao inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+var leonardo = window.document.getElementById("leonardo")
+var samanta = window.document.getElementById("samanta")
+var bruna = window.document.getElementById("bruna")
+var setadireita = window.document.getElementById("setadireita")
+var setaesquerda = window.document.getElementById("setaesquerda")
+
+function rolarparadireita() {
+    leonardo.style ="display:none"
+    bruna.style = "display:flex"
+    setadireita.style = "display:none"
+    setaesquerda.style = "display:flex; margin-top:60px"
+}
+
+function rolarparaesquerda() {
+    leonardo.style ="display:flex"
+    bruna.style ="display:none"
+    setadireita.style ="display:flex; margin-top:40px"
+    setaesquerda.style = "display:none"
+}
